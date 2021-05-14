847bc6d0bcd7df827d565eadaace60195c12e141
Lucian <lucian.miller185@gmail.com>
2021-05-14 09:43:46 -0700

    It will output list of items in a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $("form#form").submit(function(event) {
-    
+    const blanks = ["item1", "item2", "item3", "item4"];
+
+    blanks.forEach(function(blank) {
+      let userInput = $("input#" + blank).val();
+      $("#groceries").append("<li>" + userInput + "</li>");
+    });
+
+    $("#list").show();
+    $("#form").hide();
+
     event.preventDefault();
   });
 });
